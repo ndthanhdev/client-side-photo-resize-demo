3d135018f2c292f49f9d166727f158843458b5f8
Duy Thanh Nguyen <ndthanhdev@outlook.com>
2022-01-25 11:51:03 +0700

    Initial commit

---
+++ src/Slice.tsx
@@ -0,0 +1,8 @@
+import ReactCompareImage from "react-compare-image";
+import React from "react";
+
+interface Slice {}
+
+const Slice: React.FC<Slice> = (props) => {
+  return <ReactCompareImage />;
+};
